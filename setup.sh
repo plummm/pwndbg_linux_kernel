89bc12475c83dd023a823e510c24a9b2ba71ca6e
ETenal <etenal@etenal.me>
2024-02-19 20:31:05 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,7 +20,7 @@ osx() {
 
 install_apt() {
     sudo apt-get update || true
-    sudo apt-get -y install gdb python-dev python3-dev python3-pip libglib2.0-dev libc6-dbg
+    sudo apt-get -y install gdb python3-dev python3-pip libglib2.0-dev libc6-dbg
 
     if uname -m | grep x86_64 > /dev/null; then
         sudo apt-get -y install libc6-dbg:i386 || true
